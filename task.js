718fb124711b0a6be6568e918a29b7afb6521582
Romain Berger <romain@romainberger.com>
2014-01-23 01:17:46 +0100

    wat

--- task.js
+++ task.js
@@ -1,8 +1,6 @@
 var spawn = require('child_process').spawn
   , task  = process.argv[2]
 
-  console.log(task)
-
 if (task === 'build') {
   spawn('zip' , ['-r', 'app.nw', 'src', 'package.json'], {stdio: 'inherit'})
 }
